eb0919f537bf9985dac0249cf4fe1a047fdce7a7
IceProgrammer <programmer.ice@gmail.com>
2021-01-10 11:43:30 -0400

    empty template

--- web/src/utils/iotCommands.js
+++
@@ -1,8 +0,0 @@
-import {httpPost} from '.'; 
-
-const setPinUrl = 'setpin';
-
-export const setDigitalPin = async (pin, state) =>{
-    const response = await httpPost(setPinUrl,{pin,state});
-    return response;
-}
